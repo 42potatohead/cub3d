04b762a0ddd9ee62004c2e3011a16ca77f2207eb
Zaid Abu Baker <zabu-bak@z2t7c1.42bangkok.com>
2025-12-21 17:44:06 +0700

    cub3d

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cub3d
+
+
+# Compile Command
+cc main.c -I./MLX42/include -L./MLX42/build -lmlx42 -lglfw -lX11 -lXext -g
---
+++ cub3d.h
@@ -0,0 +1,18 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "MLX42/MLX42.h"
+
+#define WIDTH 512
+#define HEIGHT 512
+
+typedef struct s_mlxdata 
+{
+    mlx_t* mlx;
+
+}   t_mlxdata;
+
+#endif
